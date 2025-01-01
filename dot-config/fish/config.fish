7ece2b378baee3e5b263a398b2c22a687fce729e
Sitnikov Timofey <tima.sitnikov@mail.ru>
2025-01-01 19:22:51 +0300

    добавил комплишен для шелла

--- dot-config/fish/config.fish
+++ dot-config/fish/config.fish
@@ -56,6 +56,14 @@ end
 #
 # Initializations
 #
+if test -d (brew --prefix)"/share/fish/completions"
+    set -p fish_complete_path (brew --prefix)/share/fish/completions
+end
+
+if test -d (brew --prefix)"/share/fish/vendor_completions.d"
+    set -p fish_complete_path (brew --prefix)/share/fish/vendor_completions.d
+end
+
 if command -sq starship
     starship init fish | source
 end

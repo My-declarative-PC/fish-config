3419b62cef6017ee93c4fdaf52628cbcb8600db6
Sitnikov Timofey <tima.sitnikov@mail.ru>
2025-03-04 11:57:40 -0500

    указал явный путь к brew

--- dot-config/fish/config.fish
+++ dot-config/fish/config.fish
@@ -56,12 +56,12 @@ end
 #
 # Initializations
 #
-if test -d (brew --prefix)"/share/fish/completions"
-    set -p fish_complete_path (brew --prefix)/share/fish/completions
+if test -d (/home/linuxbrew/.linuxbrew/bin/brew --prefix)"/share/fish/completions"
+    set -p fish_complete_path (/home/linuxbrew/.linuxbrew/bin/brew --prefix)/share/fish/completions
 end
 
-if test -d (brew --prefix)"/share/fish/vendor_completions.d"
-    set -p fish_complete_path (brew --prefix)/share/fish/vendor_completions.d
+if test -d (/home/linuxbrew/.linuxbrew/bin/brew --prefix)"/share/fish/vendor_completions.d"
+    set -p fish_complete_path (/home/linuxbrew/.linuxbrew/bin/brew --prefix)/share/fish/vendor_completions.d
 end
 
 if command -sq starship
